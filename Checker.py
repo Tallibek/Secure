be642d46a7664dcf463fa32ad646f3d25c6281e2
Tallibek <iwrit0@gmail.com>
2025-09-18 02:39:37 +0200

    Zmieniono plik, dodano nową metodę check_pswd

--- Checker.py
+++ Checker.py
@@ -2,26 +2,26 @@ import hashlib
 import requests
 
 
-pswd = input("Podaj hasło do sprawdzenia: ")
-hash = hashlib.sha1(pswd.encode("utf-8")).hexdigest().upper()
-prefix = hash[:5]
-suffix = hash[5:]
+def check_pswd(pswd):
+
+    hash = hashlib.sha1(pswd.encode("utf-8")).hexdigest().upper()
+    prefix, suffix = hash[:5], hash[5:]
+    url = f"https://api.pwnedpasswords.com/range/{prefix}"
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise RuntimeError("Błąd API.")
+    hashes = (line.split(":") for line in response.text.splitlines())
+    for h, count in hashes:
+        if h == suffix:
+            return int(count)
+    return 0
 
 
-url = f"https://api.pwnedpasswords.com/range/{prefix}"
-response = requests.get(url)
-
-
-if suffix in response.text:
-    print("X To hasło wyciekło! Zmień je.")
-
-else:
-    print("V Wygląda na bezpieczne.")
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    pswd = input("Podaj hasło do sprawdzenia: ")
+    result = check_pswd(pswd)
+    if result:
+        print(f"[X] Twoje hasło pojawiło się w wyciekach {result} razy! Zmień je.")
+    else:
+        print("[V] Wygląda na to, że Twoje hasło nie pojawiło się w znanych wyciekach.")
 
